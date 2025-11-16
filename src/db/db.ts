f528f1c3e4514f60035056764fb3ad22fd383153
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-11-16 12:32:50 +0530

    init neon websocket adapter

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,10 +1,13 @@
-import { env } from '$env/dynamic/private';
 import * as schema from './schema';
-import { drizzle as drizzle_neon } from 'drizzle-orm/neon-http';
-import { neon } from '@neondatabase/serverless';
+import { drizzle as drizzle_neon } from 'drizzle-orm/neon-serverless';
+import { Pool } from '@neondatabase/serverless';
 import { get_db_url } from './db_utils';
+import type { PgTransaction } from 'drizzle-orm/pg-core';
+import type { NeonQueryResultHKT } from 'drizzle-orm/neon-serverless';
+import type { ExtractTablesWithRelations } from 'drizzle-orm';
+import type { PostgresJsQueryResultHKT } from 'drizzle-orm/postgres-js';
 
-const DB_URL = get_db_url(env);
+const DB_URL = get_db_url(process.env);
 
 const get_drizzle_instance_dev = async () => {
   // using local postgres to allow edge environment in the edge
@@ -13,6 +16,17 @@ const get_drizzle_instance_dev = async () => {
   return drizzle(postgres.default(DB_URL), { schema });
 };
 
-export const db = import.meta.env.DEV
-  ? await get_drizzle_instance_dev()
-  : drizzle_neon(neon(DB_URL), { schema });
+export const db =
+  process.env.NODE_ENV === 'development'
+    ? await get_drizzle_instance_dev()
+    : // using neon websocket adapter
+      drizzle_neon(new Pool({ connectionString: process.env.PG_DATABASE_URL }), { schema });
+
+export type transactionType =
+  | PgTransaction<NeonQueryResultHKT, typeof schema, ExtractTablesWithRelations<typeof schema>>
+  | PgTransaction<
+      PostgresJsQueryResultHKT,
+      typeof schema,
+      ExtractTablesWithRelations<typeof schema>
+    >
+  | typeof db;
