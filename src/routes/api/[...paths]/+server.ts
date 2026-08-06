59629d0267b6db6188319983f2ec8bfdbc0aa80b
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-08-06 18:11:34 +0530

    rm , add hoooks

--- src/routes/api/[...paths]/+server.ts
+++
@@ -1,5 +0,0 @@
-import { api_handler } from '~/api/api_router';
-import type { RequestHandler } from '@sveltejs/kit';
-
-export const GET: RequestHandler = ({ request }) => api_handler.fetch(request);
-export const POST: RequestHandler = ({ request }) => api_handler.fetch(request);
